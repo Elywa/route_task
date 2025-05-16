c3c1beb276ca26434b3d9fba5869e98213597546
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-05-16 20:02:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProductsApp());
+}
+
+class ProductsApp extends StatelessWidget {
+  const ProductsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
